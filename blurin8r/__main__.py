f5bc073352234de6fe9c1964377e5d8238b81d78
adi r <aramachandran@olin.edu>
2020-06-06 22:36:56 -0500

    added changes for generating new fake webcam

--- blurin8r/__main__.py
+++ blurin8r/__main__.py
@@ -3,6 +3,13 @@ import process
 import argparse
 import cv2
 import numpy as np
+import pyfakewebcam # generating fake webcam on top of v4l2loopback-utils
+
+# need to run in terminal
+# sudo apt install v4l2loopback-dkms
+# sudo modprobe -r v4l2loopback
+# sudo modprobe v4l2loopback devices=1 video_nr=20 card_label="v4l2loopback" exclusive_caps=1
+
 
 import sys
 
@@ -44,14 +51,25 @@ if __name__ == '__main__':
     else:
         # run on video by default
         cap = cv2.VideoCapture(args.cam)
+        # cap = cv2.VideoCapture('/dev/video0')
+        height, width = 720, 1280
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        cap.set(cv2.CAP_PROP_FPS, 60)
+        fake = pyfakewebcam.FakeWebcam('/dev/video20', width, height) # setup fake
 
         while True:
-            ret, img = cap.read()
-            blurred_img = blur_frame(img, net)
-            out = np.hstack((img, blurred_img))
-            cv2.imshow('out', out)
-            if cv2.waitKey(1) & 0xff == ord('q'):
-                break
+            ret, img = cap.read() # grabbing current frame
+            blurred_img = blur_frame(img, net) # Generating blurred image with optimized YOLO
+            blurred_img_rgb = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2RGB) # switch BGR2RGB
+            fake.schedule_frame(blurred_img_rgb)
+
+            # Original CV2 Displaying
+
+            # out = np.hstack((img, blurred_img))
+            # cv2.imshow('out', out)
+            # if cv2.waitKey(1) & 0xff == ord('q'):
+            #     break
 
         cap.release()
     cv2.destroyAllWindows()
